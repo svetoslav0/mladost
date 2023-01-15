38387027ca97f503023eba4687fc872cdd30ec33
Svetoslav Nedelchev <snedelchev@isimarkets.com>
2023-01-16 00:42:20 +0200

    moved dir

---
+++ Mladost/Areas/Identity/Data/MladostUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mladost.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the MladostUser class
+public class MladostUser : IdentityUser
+{
+    [PersonalData]
+    [Column(TypeName = "varchar(255)")]
+    public string FirstName { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "varchar(255)")]
+    public string LastName { get; set; }
+}
+
